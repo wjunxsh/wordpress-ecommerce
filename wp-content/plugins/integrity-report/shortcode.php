b4776756e0a831a17e02b8fa87401d5ea7a68760
anker <anker@ankerdeMacBook-Pro.local>
2023-09-14 18:58:26 +0800

    sdaf

--- wp-content/plugins/integrity-report/shortcode.php
+++ wp-content/plugins/integrity-report/shortcode.php
@@ -39,9 +39,10 @@ class IntegrityReportShortcode {
 
     public static function integrity_report_form() {
         ob_start(); // start print
-        
+        //6LfBRSAoAAAAAHaaD8l0UJkepX90x4Fi5pKMpcD3
+        //线上  
         ?>
-        <script type="text/javascript" src="https://www.recaptcha.net/recaptcha/api.js?render=6LcoVRwoAAAAACzZKSjLMwj7edwH02_Z7_bzF3Wh" async defer></script>
+        <script type="text/javascript" src="https://www.recaptcha.net/recaptcha/api.js?render=6LfBRSAoAAAAAHaaD8l0UJkepX90x4Fi5pKMpcD3" async defer></script>
         <form method="post" action="<?php echo esc_url(admin_url('admin-post.php')); ?>" enctype="multipart/form-data" class="needs-validation" novalidate>
             <input type="hidden" name="action" value="integrity_report_form">
         <?php
