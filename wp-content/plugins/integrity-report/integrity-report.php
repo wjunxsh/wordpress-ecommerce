f65b20b6364eadc0b3b9688b0ebe93dec931e9b9
anker <anker@ankerdeMacBook-Pro.local>
2023-09-14 11:21:21 +0800

    sdfasf

--- wp-content/plugins/integrity-report/integrity-report.php
+++ wp-content/plugins/integrity-report/integrity-report.php
@@ -7,10 +7,6 @@ Author: Anker DTC IT
 */
 
 // Integrity Report Agree Bar
-
-// process submit form
 define( 'INTEGRITY_REPORT_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
-require_once( INTEGRITY_REPORT_PLUGIN_DIR . 'claaa.integrity-report.backend.php' );
+require_once( INTEGRITY_REPORT_PLUGIN_DIR . 'class.integrity-report.backend.php' );
 require_once( INTEGRITY_REPORT_PLUGIN_DIR . 'class.integrity-report.front.php' );
-IntegrityReportBackend::init();
-IntegrityReportFront::init();
